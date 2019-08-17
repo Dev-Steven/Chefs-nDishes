5d396402e7c8c82bc6e9d11f32df8563f40bdb28
Steven <steven.chung96@yahoo.com>
2019-08-17 12:24:38 -0700

    Created database and tables

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ChefsNDishes.Models;
+using System.Linq;
+
+namespace ChefsNDishes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext dbContext;
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
